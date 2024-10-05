9ba08ab525aa32c545104203705dd2d7240753ae
deboradum <pvwpvw@protonmail.com>
2024-10-05 11:54:47 +0200

    gaussian smoothing

--- utils/utils.py
+++ utils/utils.py
@@ -1,11 +1,15 @@
 import os
 import cv2
+import numbers
 
 import numpy as np
+import mlx.nn as nn
 import mlx.core as mx
-import mlx.models as mlx_models
 
-from constants import *
+from models.Vgg19 import Vgg19
+from mlx.nn.layers.base import Module
+from utils.constants import IMAGENET_MEAN_1, IMAGENET_STD_1
+
 
 def load_image(img_path, target_shape=None):
     if not os.path.exists(img_path):
@@ -37,9 +41,9 @@ def load_image(img_path, target_shape=None):
 
 def get_model(model_type):
     if model_type == "vgg19":
-        model = mlx_models.vision.VGG19(load_weights=True)
-    elif model_type == "alexnet":
-        model = mlx_models.vision.AlexNet(load_weights=True)
+        model = Vgg19()
+    # elif model_type == "alexnet":
+    #     model = mlx_models.vision.AlexNet(load_weights=True)
     else:
         raise Exception(f"Model '{model_type}' not supported")
 
@@ -75,26 +79,95 @@ def mlx_input_adapter(img):
     # shape = (1, 3, H, W)
     # CHANGED TO
     # shape = (1, H, W, 3)
-    tensor = mx.expand_dims(mx.array(img), 0)
-    # tensor.requires_grad = True # TODO
-    return tensor
+    array = mx.expand_dims(mx.array(img), 0)
+    # array.requires_grad = True # TODO
+    return array
 
 
-def mlx_output_adapter(tensor):
+def mlx_output_adapter(array):
     # Push to CPU, detach from the computational graph, convert from (1, H, W, 3) into (H, W, 3)
-    tensor = np.array(tensor[0])
-    return tensor
+    array = np.array(array[0])
+    return array
 
 
-def random_circular_spatial_shift(tensor, h_shift, w_shift, should_undo=False):
+def random_circular_spatial_shift(array, h_shift, w_shift, should_undo=False):
     if should_undo:
         h_shift = -h_shift
         w_shift = -w_shift
     # with torch.no_grad():
     rolled = mx.array(
-        np.array(tensor).roll(
-            tensor, shift=(h_shift, w_shift), axis=(1, 2)
+        np.roll(
+            np.array(array), shift=(h_shift, w_shift), axis=(1, 2)
         )  # there's no MLX roll?
     )
     # rolled.requires_grad = True
     return rolled
+
+
+class CascadeGaussianSmoothing(Module):
+    """
+    Apply gaussian smoothing separately for each channel (depthwise convolution).
+
+    Arguments:
+        kernel_size (int, sequence): Size of the gaussian kernel.
+        sigma (float, sequence): Standard deviation of the gaussian kernel.
+
+    """
+
+    def __init__(self, kernel_size, sigma):
+        super().__init__()
+
+        if isinstance(kernel_size, numbers.Number):
+            kernel_size = [kernel_size, kernel_size]
+
+        cascade_coefficients = [0.5, 1.0, 2.0]  # std multipliers
+        sigmas = [
+            [coeff * sigma, coeff * sigma] for coeff in cascade_coefficients
+        ]  # isotropic Gaussian
+
+        self.pad = int(kernel_size[0] / 2)  # assure we have the same spatial resolution
+
+        # The gaussian kernel is the product of the gaussian function of each dimension.
+        kernels = []
+        meshgrids = mx.meshgrid(
+            [mx.arange(size, dtype=mx.float32) for size in kernel_size]
+        )
+        for sigma in sigmas:
+            kernel = mx.ones_like(meshgrids[0])
+            for size_1d, std_1d, grid in zip(kernel_size, sigma, meshgrids):
+                mean = (size_1d - 1) / 2
+                kernel *= (
+                    1
+                    / (std_1d * mx.sqrt(2 * mx.pi))
+                    * mx.exp(-(((grid - mean) / std_1d) ** 2) / 2)
+                )
+            kernels.append(kernel)
+
+        gaussian_kernels = []
+        for kernel in kernels:
+            # Normalize - make sure sum of values in gaussian kernel equals 1.
+            kernel = kernel / mx.sum(kernel)
+            # Reshape to depthwise convolutional weight
+            kernel = kernel.view(1, 1, *kernel.shape)
+            kernel = kernel.repeat(3, 1, 1, 1)
+
+            gaussian_kernels.append(kernel)
+
+        self.weight1 = gaussian_kernels[0]
+        self.weight2 = gaussian_kernels[1]
+        self.weight3 = gaussian_kernels[2]
+        self.conv = nn.Conv2d
+
+    def forward(self, input):
+        input = mx.pad(
+            input, [self.pad, self.pad, self.pad, self.pad], mode="constant"
+        )  # mode=reflect
+
+        # Apply Gaussian kernels depthwise over the input (hence groups equals the number of input channels)
+        # shape = (1, 3, H, W) -> (1, 3, H, W)
+        num_in_channels = input.shape[1]
+        grad1 = self.conv(input, weight=self.weight1, groups=num_in_channels)
+        grad2 = self.conv(input, weight=self.weight2, groups=num_in_channels)
+        grad3 = self.conv(input, weight=self.weight3, groups=num_in_channels)
+
+        return (grad1 + grad2 + grad3) / 3
